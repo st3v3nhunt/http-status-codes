bc6796a835efde20000d99e12e8d7c1da5ec1735
Steven Hunt <st3v3nhunt@users.noreply.github.com>
2021-09-20 22:38:08 +0100

    feat: return 400 for invalid request (#5)

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import statusCodes from "./src/status-codes.ts";
 
 function isStatusInRange(status: number): boolean {
-  return status < 200 || status > 599;
+  return status > 199 && status < 600;
 }
 
 function doesStatusCauseError(status: number): boolean {
@@ -9,12 +9,13 @@ function doesStatusCauseError(status: number): boolean {
 }
 
 function generateResponse(status: number): Response {
+  console.log("Response status:", status);
   return new Response(JSON.stringify(statusCodes[status]), {
     headers: { "content-type": "application/json; charset=UTF-8" },
     // In order to prevent errors for certain status codes and
     // only return those in valid range return a 200
     status:
-      isStatusInRange(status) || doesStatusCauseError(status) ? 200 : status,
+      !isStatusInRange(status) || doesStatusCauseError(status) ? 200 : status,
     statusText: "statusText something",
   });
 }
@@ -26,10 +27,10 @@ function getStatusCode(url: string): number {
 }
 
 async function handleRequest(request: Request): Promise<Response> {
-  // console.log(request);
+  console.log("Request:", request);
   const url = new URL(request.url);
-  console.log(url);
   const path = url.pathname;
+
   if (path === "/") {
     const file = await Deno.readFile("./assets/views/home.html");
     return new Response(file, {
@@ -48,12 +49,11 @@ async function handleRequest(request: Request): Promise<Response> {
       headers: { "content-type": "image/png" },
     });
   }
+
   const statusCode = getStatusCode(request.url);
-  if (isNaN(statusCode)) {
-    return generateResponse(400);
-  }
-  console.log("Status:", statusCode);
-  return generateResponse(statusCode);
+  return isNaN(statusCode) || !isStatusInRange(statusCode)
+    ? generateResponse(400)
+    : generateResponse(statusCode);
 }
 
 addEventListener("fetch", (event: FetchEvent) => {
