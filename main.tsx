10ae05b001d4838a3e374bd5eda2b1bd2c08de03
Steve Hunt <st3v3nhunt@users.noreply.github.com>
2022-05-27 17:15:26 +0100

    feat: time requests

--- main.tsx
+++ main.tsx
@@ -1,6 +1,6 @@
 import statusCodes from "./src/status-codes.ts";
-import { h } from "https://x.lcas.dev/preact@10.5.12/mod.js";
-import { renderToString } from "https://x.lcas.dev/preact@10.5.12/ssr.js";
+import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import { renderToString } from "https://esm.sh/preact-render-to-string@5.1.19?deps=preact@10.5.15";
 
 function isStatusInRange(status: number): boolean {
   return status > 199 && status < 600;
@@ -11,20 +11,19 @@ function doesStatusCauseError(status: number): boolean {
 }
 
 function generateResponse(status: number): Response {
-  console.log("Response status:", status);
   return new Response(JSON.stringify(statusCodes[status]), {
     headers: { "content-type": "application/json; charset=UTF-8" },
     // In order to prevent errors for certain status codes and
     // only return those in valid range return a 200
     status: !isStatusInRange(status) || doesStatusCauseError(status)
-      ? 200
-      : status,
+    ? 200
+    : status,
     statusText: "statusText something",
   });
 }
 
-function getStatusCode(url: string): number {
-  const { pathname } = new URL(url);
+function getStatusCode(url: URL): number {
+  const { pathname } = url;
   const statusRaw = pathname.split("/")[1];
   return parseInt(statusRaw, 10);
 }
@@ -32,45 +31,51 @@ function getStatusCode(url: string): number {
 function App() {
   return (
     <html lang="en">
-      <head>
-        <title>HTTP Status Codes</title>
-      </head>
-      <body>
-        <h1>HTTP Status Codes</h1>
-      </body>
+    <head>
+    <title>HTTP Status Codes</title>
+    </head>
+    <body>
+    <h1>HTTP Status Codes</h1>
+    <p>{new Date().toLocaleString()}</p>
+    </body>
     </html>
   );
 }
 
 async function handleRequest(request: Request): Promise<Response> {
+  const start = performance.now()
   console.log("Request:", request);
   const url = new URL(request.url);
   const path = url.pathname;
+  let response
 
   if (path === "/") {
-    return new Response("<!DOCTYPE html>" + renderToString(<App />), {
+    performance.mark('b')
+    response = new Response(renderToString(App()), {
       headers: { "content-type": "text/html" },
     });
   } else if (path.endsWith(".ico")) {
     const iconPath = path.split("/").pop();
     const file = await Deno.readFile(`./assets/img/${iconPath}`);
-    return new Response(file, {
+    response = new Response(file, {
       headers: { "content-type": "image/x-icon" },
     });
   } else if (path.endsWith(".png")) {
     const imagePath = path.split("/").pop();
     const file = await Deno.readFile(`./assets/img/${imagePath}`);
-    return new Response(file, {
+    response = new Response(file, {
       headers: { "content-type": "image/png" },
     });
+  } else {
+    const statusCode = getStatusCode(url);
+    response = isNaN(statusCode) || !isStatusInRange(statusCode)
+      ? generateResponse(400)
+      : generateResponse(statusCode);
   }
-
-  const statusCode = getStatusCode(request.url);
-  return isNaN(statusCode) || !isStatusInRange(statusCode)
-    ? generateResponse(400)
-    : generateResponse(statusCode);
+  const end = performance.now()
+  console.log(`Request for ${path} took ${end-start} milliseconds.`)
+  return response
 }
 
-addEventListener("fetch", (event: FetchEvent) => {
-  event.respondWith(handleRequest(event.request));
-});
+serve(handleRequest)
+console.log('Listening on port 8000')
